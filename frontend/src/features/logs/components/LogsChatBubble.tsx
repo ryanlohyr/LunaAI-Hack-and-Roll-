8af2bf76667dfb98271fab9a9f10c116fd82223f
mingyuanc <cmingyuan123@gmail.com>
2024-01-21 07:05:02 +0800

    Working version

--- frontend/src/features/logs/components/LogsChatBubble.tsx
+++ frontend/src/features/logs/components/LogsChatBubble.tsx
@@ -2,17 +2,17 @@ import { cn } from '@/lib/utils';
 import React, { RefObject } from 'react'
 
 type Props = {
-    role: string;
     content: string;
+    index: number;
 }
 
 const LogsChatBubble = (props: Props) => {
-
+    // console.log(props.index % 1 === 0)
     return (
         <div 
             className={cn(
                 'self-end bg-white max-w-96 p-3',
-                props.role === "user" ? "self-start bg-lunaPrimary text-white rounded-br-lg" : "self-end bg-white rounded-tl-md border border-gray-300"
+                props.index % 2 === 1 ? "self-start bg-lunaPrimary text-white rounded-br-lg" : "self-end bg-white rounded-tl-md border border-gray-300"
 
             )}
         >
