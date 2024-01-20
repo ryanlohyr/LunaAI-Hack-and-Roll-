47681bbf1be92a4612f0c11cc41f2d78c8c19f8a
mingyuanc <cmingyuan123@gmail.com>
2024-01-21 02:08:24 +0800

    Update upsert model

--- backend/pinecone/classes/app_types.py
+++ backend/pinecone/classes/app_types.py
@@ -3,19 +3,14 @@ from typing import List
 
 
 class CreateIndex(BaseModel):
-  index_name: str
+    index_name: str
+
 
 class Upsert(BaseModel):
-  data_arr: List = [
-    {
-      "id": str,
-      "content": str,
-      "metadata": {
-        "header": str
-      }
+    data_arr: List = [
+        {"id": str, "content": str, "metadata": {"header": str, "content": str}}
+    ]
 
-    }
-  ]
 
 class Query(BaseModel):
-  question: str
+    question: str
