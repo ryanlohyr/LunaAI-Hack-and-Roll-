84e066a4cc1c7cef7a7d15089acde913c6a19b25
Ryan Loh <ryanloh29@gmail.com>
2024-01-21 07:33:13 +0800

    Add voice service

--- backend/VoiceService/functions/transcribe.js
+++ backend/VoiceService/functions/transcribe.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+
 exports.handler = function (context, event, callback) {
     // Create a TwiML Voice Response object to build the response
     const twiml = new Twilio.twiml.VoiceResponse();
@@ -5,23 +7,13 @@ exports.handler = function (context, event, callback) {
     // If no previous conversation is present, or if the conversation is empty, start the conversation
     if (!event.request.cookies.convo) {
         // Greet the user with a message using AWS Polly Neural voice
-        twiml.say({
-            voice: 'Polly.Joanna-Neural',
-        },
-            "Hey! I'm Luna, the official hack and roll chatbot. I can help you with any questions you have about the CPF. Press one for English, or two for Chinese."
-        );
-        // Greet the user with a message using AWS Polly Neural voice
-        twiml.say({
-            voice: 'Polly.Zhiyu-Neural',
-        },
-            "你好！我是Luna，官方的Hack and Roll聊天机器人。如果您对CPF有任何疑问，我可以帮助您。按1为英语，或按2为中文。"
-        );
+        twiml.play('https://ryanloh.vercel.app/nova.mp3');
 
         twiml.gather({
             input: 'dtmf', // Specify dtmf as the input type
             numDigits: 1, // Gather 1 digit
             action: '/processInput', // Send the collected input to /processInput 
-        });  
+        });
     }
 
     // Listen to the user's speech and pass the input to the /respond Function
@@ -40,6 +32,7 @@ exports.handler = function (context, event, callback) {
 
     // Set the response body to the generated TwiML
     response.setBody(twiml.toString());
+
 
     // // If no conversation cookie is present, set an empty conversation cookie
     // if (!event.request.cookies.convo) {
